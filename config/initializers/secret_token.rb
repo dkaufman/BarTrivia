053478e57ee37903691442cd34fca410c6b297a1
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-06-19 17:59:49 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LsTrivia::Application.config.secret_token = '3104e114511f3a5d5c08cba3385116e079830484813ece29d3358cd4e0e87b8f5f1d0d1c0003d40f2e2ac055cdf4ed8abd1570f784461b80a8d42acb053184fa'

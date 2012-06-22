25521529515e1a0b060eeed78a4e051755ac79ca
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-06-21 22:30:34 -0400

    Still Moving Pusher

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,4 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require 'pusher'
 
-Pusher.app_id = 22686
-Pusher.key = '7f1590723727b1a008b1'
-Pusher.secret = '4fa63da43b54cd350d48'
-
-# Initialize the rails application
 LsTrivia::Application.initialize!

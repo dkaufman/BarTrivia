0f09fba2f7ae69061b115df96e9e5d444ac7b242
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-06-22 08:52:15 -0400

    Re-added pusher config to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'pusher'
 
 LsTrivia::Application.initialize!
+
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
